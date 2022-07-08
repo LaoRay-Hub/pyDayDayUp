e09373bb2fdbe90bbd4a80a449c40b2da0bfd226
LaoRay <zhengyuanlei@gmail.com>
2022-07-08 20:52:15 +0800

    test

--- test_02.py
+++ test_02.py
@@ -5,3 +5,24 @@ print("hello")  # 注释2
 """
 多行注释
 """
+print("_" * 10)
+
+qq_number: int = 123456
+print(qq_number)
+qq_number: str = '1234'
+print(qq_number)
+
+a: bool = True
+
+b: float = 1.00
+print(b)
+
+c = type(a)
+print(c)
+
+d = type(c)
+print(d)
+
+print("2"+"1")
+
+
